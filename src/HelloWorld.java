05d50d998c5a573c07ac54b684cc3313bb188b0f
1E10 Laptop <sample@bths.edu>
2022-09-21 12:20:35 -0400

    Added another variable and a remainder of that

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String[]args){
+       int x = 29;
+       int y = 8;
+       int z = 9;
+       int remainder = x%y;
+       int remainder2 = x%z;
+        System.out.println("The remainder of " + x + " divided by " + y + " is " + remainder);
+        System.out.println("The remainder of " + x + " divided by " + z + " is " + remainder2);
+    }
 }
